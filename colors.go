aeba90bb232d1594c5738119d1632a40480572f7
blakehulett7 <blake.hulett7@gmail.com>
2025-08-15 16:02:07 -0500

    added yellow

--- colors.go
+++ colors.go
@@ -8,6 +8,7 @@ const BrightGreen = "\033[92m"
 const Cyan = "\033[36m"
 const Green = "\033[32m"
 const Red = "\033[31m"
+const Yellow = "\033[33m"
 
 func ColorPrint(color, message string) {
 	fmt.Printf("%s%s%s\n", color, message, ResetColor)
